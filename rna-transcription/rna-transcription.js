d2768fc281811a06a456e8020e709be66dad3609
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-19 18:03:16 +0300

    RNA transcription SOLVED

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-	throw new Error('Remove this statement and implement this function');
-};
+const dna2rna = new Map([['C', 'G'], ['G', 'C'], ['T', 'A'], ['A', 'U']])
+
+export const toRna = ([...sequence]) => sequence.map(nucleotide => dna2rna.get(nucleotide)).join('')
