38ae1f150d49a816df9bf864753bb6228e111b96
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-19 23:13:52 +0300

    Linked list SOLVED

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -3,28 +3,72 @@
 // convenience to get you started writing code faster.
 //
 
+class Node {
+  constructor(value) {
+    this.value = value
+    this.prev = null
+    this.next = null
+  }
+  link(node) {
+    this.next = node
+    node.prev = this
+  }
+  reset() {
+    this.prev = null
+    this.next = null
+  }
+}
+
 export class LinkedList {
-  push() {
-    throw new Error('Remove this statement and implement this function');
+  #head = null
+  #tail = null
+  #counter = 0
+
+  push(value) {
+    let node = new Node(value)
+    if (this.#tail) { node.prev = this.#tail; this.#tail.next = node; this.#tail = node }
+    else this.#tail = this.#head = node
+    this.#counter++
   }
 
   pop() {
-    throw new Error('Remove this statement and implement this function');
+    let gift = this.#tail
+    if (this.#tail == this.#head) this.#tail = this.#head = null
+    else { this.#tail = this.#tail.prev; this.#tail.next = null }
+    this.#counter--
+    return gift.value
   }
 
   shift() {
-    throw new Error('Remove this statement and implement this function');
+    let gift = this.#head
+    if (this.#head == this.#tail) this.#head = this.#tail = null
+    else { this.#head = this.#head.next; this.#head.prev = null }
+    this.#counter--
+    return gift.value
   }
 
-  unshift() {
-    throw new Error('Remove this statement and implement this function');
+  unshift(value) {
+    let node = new Node(value)
+    if (this.#head) { node.next = this.#head; this.#head.prev = node; this.#head = node }
+    else this.#head = this.#tail = node
+    this.#counter++
   }
 
-  delete() {
-    throw new Error('Remove this statement and implement this function');
+  delete(value) {
+    if (this.#head.value == value) return this.shift()
+    if (this.#counter <= 1) return
+    for (let current = this.#head.next; current != this.#tail; current = current.next) {
+      if (current.value == value) {
+        current.prev.next = current.next
+        current.next.prev = current.prev
+        this.#counter--
+        return
+      }
+    }
+    if (this.#tail.value == value) return this.pop()
   }
 
   count() {
-    throw new Error('Remove this statement and implement this function');
+    return this.#counter
   }
 }
