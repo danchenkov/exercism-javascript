c9ab3c09d60c4984e7728895d0539820f00414b7
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-19 08:13:57 +0300

    Mixed juices SOLVED

--- mixed-juices/mixed-juices.js
+++ mixed-juices/mixed-juices.js
@@ -11,7 +11,19 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+    case 'Energizer':
+    case 'Green Garden':
+      return 1.5
+    case 'Tropical Island':
+      return 3
+    case 'All or Nothing':
+      return 5
+    default:
+      return 2.5
+  }
 }
 
 /**
@@ -23,7 +35,22 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+  let counter = 0;
+  while (limes.length > 0 && wedgesNeeded > 0) {
+    switch (limes.shift()) {
+      case 'small':
+        wedgesNeeded -= 6;
+        break;
+      case 'medium':
+        wedgesNeeded -= 8;
+        break;
+      case 'large':
+        wedgesNeeded -= 10
+        break;
+    }
+    counter++;
+  }
+  return counter
 }
 
 /**
@@ -34,5 +61,8 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  while (timeLeft > 0) {
+    timeLeft -= timeToMixJuice(orders.shift() || '')
+  }
+  return orders
 }
