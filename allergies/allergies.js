fb81a95ad48274c2e97a7311b82af78deb9af7ed
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-20 12:40:33 +0300

    Allergies SOLVED

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -3,16 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
+const AllergiesList = [
+  'eggs',
+  'peanuts',
+  'shellfish',
+  'strawberries',
+  'tomatoes',
+  'chocolate',
+  'pollen',
+  'cats'
+]
+
 export class Allergies {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  #allergies
+  constructor(packed) {
+    this.#allergies = packed.toString(2).split('').reverse()
   }
 
-  list() {
-    throw new Error('Remove this statement and implement this function');
-  }
-
-  allergicTo() {
-    throw new Error('Remove this statement and implement this function');
-  }
+  list = () => AllergiesList.filter(e => this.allergicTo(e))
+  allergicTo = query => AllergiesList.indexOf(query) > -1 && (this.#allergies[AllergiesList.indexOf(query)] == 1)
 }
