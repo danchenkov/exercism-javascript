7e31298552a63b51e3bea440c6094f6038409d6f
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-20 14:30:33 +0300

    Lasagna master SOLVED

---
+++ lasagna-master/lasagna-master.js
@@ -0,0 +1,37 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+export function cookingStatus(timer) {
+	switch (timer) {
+		case undefined:
+			return 'You forgot to set the timer.'
+		case 0:
+			return 'Lasagna is done.'
+		default:
+			return 'Not done, please wait.'
+	}
+}
+
+export function preparationTime(layers, timePerLayer = 2) {
+	return layers.length * timePerLayer
+}
+
+export function quantities(layers) {
+	let quantities = { 'noodles': 0, 'sauce': 0 }
+	layers.map((layer) => {
+		if (layer === 'sauce') { quantities['sauce'] += 0.2 }
+		if (layer === 'noodles') { quantities['noodles'] += 50 }
+	})
+	return quantities
+}
+
+export function addSecretIngredient(friendsList, myList) {
+	myList.push(friendsList[friendsList.length - 1])
+}
+
+export function scaleRecipe(recipe, scale) {
+	return Object.entries(recipe).reduce((acc, [key, value]) => {
+		acc[key] = typeof value === 'number' ? value * scale / 2 : value;
+		return acc;
+	}, {})
+}
