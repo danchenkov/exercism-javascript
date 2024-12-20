21c012aa930d31627a93d1e7e3950e5cd894960c
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-20 09:18:56 +0300

    Leap year SOLVED

--- leap/leap.js
+++ leap/leap.js
@@ -3,6 +3,4 @@
 // convenience to get you started writing code faster.
 //
 
-export const isLeap = () => {
-	throw new Error('Remove this statement and implement this function');
-};
+export const isLeap = year => (year % 400 === 0 || year % 100 !== 0 && year % 4 === 0);
