a07211c899ff47be69f6d2b76cd382ac435a726a
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-20 09:27:31 +0300

    Collatz conjecture SOLVED

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const steps = (n) => {
+  let counter = 0
+
+  if (Number.isNaN(n) || n <= 0) throw new Error('Only positive numbers are allowed')
+
+  for (; n !== 1; counter++) {
+    if (n % 2) n = 3 * n + 1
+    else n = n / 2
+  }
+
+  return counter
+}
