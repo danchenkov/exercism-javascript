b1166ef08c4c0daa989de5b4ff9de0072a038c68
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-20 09:47:13 +0300

    Clock SOLVED

--- clock/clock.js
+++ clock/clock.js
@@ -3,24 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
+const MINUTES_IN_AN_HOUR = 60
+const HOURS_IN_A_DAY = 24
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hours, minutes = 0) {
+    this.minutes = hours * MINUTES_IN_AN_HOUR + minutes
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    let h = Math.floor(this.minutes / MINUTES_IN_AN_HOUR) % HOURS_IN_A_DAY
+    if (h < 0) h += HOURS_IN_A_DAY
+    let m = this.minutes % MINUTES_IN_AN_HOUR
+    if (m < 0) m += MINUTES_IN_AN_HOUR
+    return `${h.toString(10).padStart(2, '0')}:${m.toString(10).padStart(2, '0')}`
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(minutes) {
+    this.minutes += minutes
+    return this
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minutes) {
+    this.minutes -= minutes
+    return this
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  equals(time) {
+    return this.toString() === time.toString()
   }
 }
