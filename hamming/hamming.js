8058321fd256095d674449a076f4e4697cf23904
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-20 10:47:16 +0300

    Hamming updated SOLVED

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -4,8 +4,6 @@
 //
 
 export const compute = (strandA, strandB) => {
-  if (!strandA.length && strandB.length) throw new Error('left strand must not be empty')
-  if (strandA.length && !strandB.length) throw new Error('right strand must not be empty')
-  if (strandA.length !== strandB.length) throw new Error('left and right strands must be of equal length')
+  if (strandA.length !== strandB.length) throw new Error('strands must be of equal length')
   return strandA.split('').reduce((hammingDistance, nucleotide, index) => hammingDistance + (strandB[index] !== nucleotide), 0)
 }
