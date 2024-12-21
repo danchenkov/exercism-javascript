9fd3bd4ff0ae8fc42378f6d38982754c5b15fcc1
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-21 15:05:15 +0300

    Protein translation SOLVED v2

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,42 +1,15 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const POLYPEPTIDES = [
+  "STOP", "Methionine", "Phenylalanine", "Leucine", "Serine", "Tyrosine", "Cysteine", "Tryptophan"
+]
 
-export const translate = (rna) => {
-  if (!rna) return [];
-  if (rna.match(/[^ACGU]/g) || rna.length % 3) throw new Error('Invalid codon');
-
-  const POLYPEPTIDES = [
-    "STOP", "Methionine", "Phenylalanine", "Leucine", "Serine", "Tyrosine", "Cysteine", "Tryptophan"
-  ]
-  const CODONS = {
-    "AUG": 1,
-    "UUU": 2,
-    "UUC": 2,
-    "UUA": 3,
-    "UUG": 3,
-    "UCU": 4,
-    "UCC": 4,
-    "UCA": 4,
-    "UCG": 4,
-    "UAU": 5,
-    "UAC": 5,
-    "UGU": 6,
-    "UGC": 6,
-    "UGG": 7,
-    "UAA": 0,
-    "UAG": 0,
-    "UGA": 0,
-  };
-
-  let sequence = []
-  let codons = rna.match(/...?/g)
-
-  for (let i = 0; i < codons.length; i++) {
-    if (!CODONS[codons[i]]) break
-    sequence.push(POLYPEPTIDES[CODONS[codons[i]]])
-  }
-
-  return sequence
+const CODONS = {
+  "AUG": 1, "UUU": 2, "UUC": 2, "UUA": 3, "UUG": 3, "UCU": 4, "UCC": 4, "UCA": 4, "UCG": 4, "UAU": 5, "UAC": 5, "UGU": 6, "UGC": 6, "UGG": 7, "UAA": 0, "UAG": 0, "UGA": 0,
 };
+
+export const translate = (rna) => (rna ? rna.match(/..?.?/g) : []).reduce((sequence, codon) => {
+  if ((sequence.stop) || (codon.length === 0)) return sequence
+  if ((CODONS[codon] === undefined) || (codon.length !== 3)) throw new Error('Invalid codon')
+  if (!CODONS[codon]) sequence.stop = true
+  else sequence.codons.push(POLYPEPTIDES[CODONS[codon]])
+  return sequence
+}, { codons: [], stop: false }).codons
