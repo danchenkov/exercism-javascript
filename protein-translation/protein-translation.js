5104b53e274e9a5d319c5a8c03dc1fcd8e2689d9
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-21 20:59:26 +0300

    Protein translation SOLVED v3 (mentor)

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,15 +1,17 @@
-const POLYPEPTIDES = [
-  "STOP", "Methionine", "Phenylalanine", "Leucine", "Serine", "Tyrosine", "Cysteine", "Tryptophan"
-]
+const PROTEINS = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine', UUC: 'Phenylalanine',
+  UUA: 'Leucine', UUG: 'Leucine',
+  UCU: 'Serine', UCC: 'Serine', UCA: 'Serine', UCG: 'Serine',
+  UAU: 'Tyrosine', UAC: 'Tyrosine',
+  UGU: 'Cysteine', UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: 'STOP', UAG: 'STOP', UGA: 'STOP',
+  get ERROR() { throw new Error('Invalid codon'); },
+}
 
-const CODONS = {
-  "AUG": 1, "UUU": 2, "UUC": 2, "UUA": 3, "UUG": 3, "UCU": 4, "UCC": 4, "UCA": 4, "UCG": 4, "UAU": 5, "UAC": 5, "UGU": 6, "UGC": 6, "UGG": 7, "UAA": 0, "UAG": 0, "UGA": 0,
-};
-
-export const translate = (rna) => (rna ? rna.match(/..?.?/g) : []).reduce((sequence, codon) => {
-  if ((sequence.stop) || (codon.length === 0)) return sequence
-  if ((CODONS[codon] === undefined) || (codon.length !== 3)) throw new Error('Invalid codon')
-  if (!CODONS[codon]) sequence.stop = true
-  else sequence.codons.push(POLYPEPTIDES[CODONS[codon]])
-  return sequence
-}, { codons: [], stop: false }).codons
+export const translate = (codons = '') =>
+  codons.match(/(.{1,3})/g)?.reduce(([proteins, stopped], codon) =>
+    (stopped || PROTEINS[codon] === 'STOP') ? [proteins, true] :
+      [[...proteins, PROTEINS[codon] || PROTEINS.ERROR], false],
+    [[], false])[0] || [];
