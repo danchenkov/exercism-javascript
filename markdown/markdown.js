8b393318fa5e7bc5b9f0b1b989b61582ad1f48a9
Alexei Danchenkov <adanchenkov@emailline.net>
2024-12-22 14:00:34 +0300

    Markdown SOLVED v3 mentor’s

--- markdown/markdown.js
+++ markdown/markdown.js
@@ -1,54 +1,10 @@
-const wrap = (text, tag) => `<${tag}>${text}</${tag}>`
-const parser = (markdown, delimiter, tag) => {
-  const pattern = new RegExp(`${delimiter}(.+)${delimiter}`);
-  const replacement = `<${tag}>$1</${tag}>`;
-  return markdown.replace(pattern, replacement);
-}
-const parse__ = (markdown) => parser(markdown, '__', 'strong')
-const parse_ = (markdown) => parser(markdown, '_', 'em')
-const parseText = (markdown, list) => {
-  const parsedText = parse_(parse__(markdown));
-  return list ? parsedText : wrap(parsedText, 'p')
-}
-const countStartingHashes = (str) => {
-  const match = str.match(/^(#\1)*/);
-  return match ? match[0].length : 0;
-}
-
-const parseHeader = (markdown, list) => {
-  let count = countStartingHashes(markdown)
-  if (count === 0 || count > 6) return [null, list];
-  const headerTag = `h${count}`;
-  const headerHtml = wrap(markdown.substring(count + 1), headerTag);
-  return list ? [`</ul>${headerHtml}`, false] : [headerHtml, false]
-}
-
-const parseLineItem = (markdown, list) => {
-  if (markdown.startsWith('*')) {
-    const innerHtml = wrap(parseText(markdown.substring(2), true), 'li');
-    return list ? [innerHtml, true] : [`<ul>${innerHtml}`, true]
-  }
-  return [null, list];
-}
-
-const parseParagraph = (markdown, list) => list ? [`</ul>${parseText(markdown, false)}`, false] : [parseText(markdown, false), false]
-
-const parseLine = (markdown, list) => {
-  let [result, inListAfter] = parseHeader(markdown, list);
-  if (result === null) [result, inListAfter] = parseLineItem(markdown, list);
-  if (result === null) [result, inListAfter] = parseParagraph(markdown, list);
-  if (result === null) throw new Error('Invalid markdown');
-  return [result, inListAfter];
-}
-
-export function parse(markdown) {
-  const lines = markdown.split('\n');
-  let result = '';
-  let list = false;
-  for (let i = 0; i < lines.length; i++) {
-    let [lineResult, newList] = parseLine(lines[i], list);
-    result += lineResult;
-    list = newList;
-  }
-  return list ? result + '</ul>' : result;
-}
+export const parse = (markdown) =>
+  markdown.replace(/(__?)(.*?)\1/g, (_, mod, text) => ({
+    '_': `<em>${text}</em>`, '__': `<strong>${text}</strong>`
+  })[mod])
+    .replace(/(?:^|\n)(#{1,6} |\* |)([^\n]+)/g, (_, mod, text) => ({
+      '* ': `<li>${text}</li>`, '# ': `<h1>${text}</h1>`, '## ': `<h2>${text}</h2>`,
+      '### ': `<h3>${text}</h3>`, '#### ': `<h4>${text}</h4>`,
+      '##### ': `<h5>${text}</h5>`, '###### ': `<h6>${text}</h6>`
+    })[mod] ?? `<p>${text}</p>`)
+    .replace(/(<li>(?!\n\n).*<\/li>)/gm, '<ul>$1</ul>');
